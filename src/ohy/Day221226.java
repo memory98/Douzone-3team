1a8855adaaa16ddfd990de27c2a07accdeef6e53
Oh-HyunYoung <gusdud2709@gmail.com>
2022-12-27 16:20:31 +0900

    Day221226 #1

--- src/ohy/Day221226.java
+++ src/ohy/Day221226.java
@@ -1,26 +1,28 @@
 package ohy;
 
+import java.util.Scanner;
+
 public class Day221226 {
 
-	public static void main(String[] args) {
-		
+	public static int main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String t = scanner.nextLine();
+		String p = scanner.nextLine();
+		return Solution(t,p);
 	}
-	class Solution {
-	    public int solution(String t, String p) {
-	        int answer = 0;
-	        int t_len = t.length();
-	        int p_len = p.length();
-	        
-	       for(int i = 0; i < t_len-p_len+1; i++){
-	            if (Long.valueOf(t.substring(i,i+p_len)) <= Long.valueOf(p)){
-	                answer++;
-	            }
-	        }
-	        return answer;
-	    }
-	}
-	// p보다 작거나 같은 수 찾기
-	// Integer.valueOf : String -> int
-	// substring : 문자열 자르기
 
+
+
+private static int Solution(String t, String p) {
+        int answer = 0;
+        int t_len = t.length();
+        int p_len = p.length();
+        
+       for(int i = 0; i < t_len-p_len+1; i++){
+            if (Long.valueOf(t.substring(i,i+p_len)) <= Long.valueOf(p)){
+                answer++;
+            }
+        }
+        return answer;
+}
 }
