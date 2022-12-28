4eb5475cfa1a7491a7c34dfc0b5a16ddfad30af1
승학산-02 <-02@DESKTOP-O5G9MTD>
2022-12-28 17:25:51 +0900

    Merge branch 'master' of https://github.com/memory98/Douzone-3team.git

--- src/ksr/Day221227.java
+++ src/ksr/Day221227.java
@@ -1,24 +1,26 @@
 package ksr;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Day221227 {
 
 	public static void main(String[] args) {
-		long result = solution(2,4);
+		long result = solution(1,5);
 		System.out.println(result);
 
 	}
 	
 	public static long solution(int k, int d) {
 	    long answer = 0;
-	    
+
 	    for(int i=0; i<=d ; i+=k){
-	        for(int j=0; j<=d; j+=k){
-	            if(((i*i)+(j*j)) > (d*d)){
-	                continue;
-	            }
-	            answer ++;
-	        }
+	    	
+	    	//answer += (int)Math.sqrt((long )(d + i) * (long)(d - i)) / k + 1;
+	        //answer += (int)Math.sqrt((long)(d * d) - (long)(i * i)) / k + 1;
+	    	answer += (Math.sqrt((d*d)-(i*i)) / k)+1;
 	    }
+
 	    return answer;
 	}
 
