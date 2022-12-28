b7136a48a41ca9298580aaa4c9126b94b909c2a1
memory98 <asdlkjh252567@gmail.com>
2022-12-28 17:59:46 +0900

    CodingTest

--- src/jes/Day221226_02.java
+++ src/jes/Day221226_02.java
@@ -5,29 +5,19 @@ public class Day221226_02 {
 //		Solution solution = new Solution();
 
 		long answer;
-		answer = solution1(2, 4);
+		answer = solution(2, 4);
 		System.out.println(answer);
-		answer = solution1(1, 5);
+		answer = solution(1, 5);
 		System.out.println(answer);
 	}
-
+	
 	public static long solution(int k, int d) {
 		long answer = 0;
-		double result;
+		
 		for (int i = 0; i <= d; i += k) {
-			for (int j = 0; j <= d; j += k) {
-				result = Math.sqrt(i * i + j * j);
-				if (d >= result) {
-					answer += 1;
-				}
-			}
-		}
-		return answer;
-	}
-	public static long solution1(int k, int d) {
-		long answer = 0;
-		for (int i = 0; i <= d; i += k) {
-			answer += ((int)(Math.sqrt(d*d-i*i))/k)+1;			
+			long len = (long)d*d -(long) i*i;
+			answer += ((long)Math.sqrt(len))/k+1;
+			System.out.println("len : ["+len+"], answer : ["+answer+"]"+", Math.sqrt(len) : "+Math.sqrt(len)+", (long)Math.sqrt(len) : "+(long)Math.sqrt(len));
 		}
 		return answer;
 	}
